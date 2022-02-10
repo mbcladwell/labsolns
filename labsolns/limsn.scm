7e41cc38e5237e96b0b80ecf8de62a20946a891a
mbcladwell <mbcladwell@gmail.com>
2022-02-10 14:22:06 -0500

    replaced lnpg with limsn

--- labsolns/limsn.scm
+++ labsolns/limsn.scm
@@ -1,39 +1,88 @@
-(define-module (labsolns lnpg)
-  #:use-module (guix packages)
+ 
+  (define-module (limsn)
   #:use-module ((guix licenses) #:prefix license:)
-  #:use-module (guix download)
-  #:use-module (guix build-system gnu)
   #:use-module (gnu packages)
-  #:use-module (gnu packages base)
-  
-  #:use-module (gnu packages databases)
- ;; #:use-module (labsolns artanis-051)
-  #:use-module (gnu packages autotools)
-  #:use-module (gnu packages guile)
   #:use-module (gnu packages guile-xyz)
-  #:use-module (gnu packages pkg-config)
-  #:use-module (gnu packages maths)
-  #:use-module (guix git-download)
-  #:use-module (gnu packages texinfo)
+  #:use-module (gnu packages algebra)
+  #:use-module (gnu packages aspell)
+  #:use-module (gnu packages autotools)
+  #:use-module (gnu packages avahi)
+  #:use-module (gnu packages base)
   #:use-module (gnu packages bash)
-   #:use-module (guix profiles)
-  #:use-module (gnu packages linux)
-  #:use-module (gnu packages databases)
-  #:use-module (gnu packages nss)
-  #:use-module (gnu packages tls)
-  #:use-module (gnu packages libffi)
+  #:use-module (gnu packages compression)
   #:use-module (gnu packages crypto)
-#:use-module (guix profiles)
-#:use-module (gnutls))
+  #:use-module (gnu packages databases)
+  #:use-module (gnu packages disk)
+  #:use-module (gnu packages emacs)
+  #:use-module (gnu packages emacs-xyz)
+  #:use-module (gnu packages gawk)
+  #:use-module (gnu packages gettext)
+  #:use-module (gnu packages gl)
+  #:use-module (gnu packages glib)
+  #:use-module (gnu packages gnome)
+  #:use-module (gnu packages gnupg)
+  #:use-module (gnu packages gperf)
+  #:use-module (gnu packages gstreamer)
+  #:use-module (gnu packages gtk)
+  #:use-module (gnu packages guile)
+  #:use-module (gnu packages hurd)
+  #:use-module (gnu packages image)
+  #:use-module (gnu packages imagemagick)
+  #:use-module (gnu packages libffi)
+  #:use-module (gnu packages libunistring)
+  #:use-module (gnu packages linux)
+  #:use-module (gnu packages man)
+  #:use-module (gnu packages maths)
+  #:use-module (gnu packages mes)
+  #:use-module (gnu packages multiprecision)
+  #:use-module (gnu packages ncurses)
+  #:use-module (gnu packages networking)
+  #:use-module (gnu packages noweb)
+  #:use-module (gnu packages nss)
+  #:use-module (gnu packages package-management)
+  #:use-module (gnu packages password-utils)
+  #:use-module (gnu packages perl)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages python)
+  #:use-module (gnu packages readline)
+  #:use-module (gnu packages sdl)
+  #:use-module (gnu packages search)
+  #:use-module (gnu packages serialization)
+  #:use-module (gnu packages slang)
+  #:use-module (gnu packages sqlite)
+  #:use-module (gnu packages swig)
+  #:use-module (gnu packages tex)
+  #:use-module (gnu packages texinfo)
+  #:use-module (gnu packages tls)
+  #:use-module (gnu packages version-control)
+  #:use-module (gnu packages webkit)
+  #:use-module (gnu packages xdisorg)
+  #:use-module (gnu packages xorg)
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (guix git-download)
+  #:use-module (guix hg-download)
+  #:use-module (guix build-system cmake)
+  #:use-module (guix build-system glib-or-gtk)
+  #:use-module (guix build-system gnu)
+  #:use-module (guix build-system guile)
+  #:use-module (guix utils)
+  #:use-module ((guix build utils) #:select (alist-replace))
+  #:use-module (ice-9 match)
+;  #:use-module (artanis artanis)
+;  #:use-module (artanis utils)
+;  #:use-module (artanis irregex)
+;  #:use-module (artanis config)
+;  #:use-module (redis)
+;  #:use-module (json)
+  
+  #:use-module (dbi dbi)
+ #:use-module ((srfi srfi-1) #:select (alist-delete)))
 
-
-
-
-
-(define-public artanis-051
+(define-public artanis-052
   (package
     (name "artanis")
-    (version "0.5.1")
+    (version "0.5.2")
     (source (origin
               (method url-fetch)
               (uri (string-append "mirror://gnu/artanis/artanis-0.5.tar.gz"))
@@ -90,6 +139,39 @@
 		   (substitute* "artanis/artanis.scm"
 		   		(("               static-page-emitter\n")
 		   		  "               static-page-emitter\n               current-myhost\n"))
+	;;============START forguix mods=========================================================================
+	
+		   (substitute* "artanis/commands/work.scm"			      			       
+				(("\\(let \\(\\(entry \\(string-append \\(current-toplevel\\) \"/\" \\*artanis-entry\\*\\)\\)\\)")
+				 "(let ((entry (string-append (original-current-toplevel) \"/\" *artanis-entry*)))")
+				(("\\(add-to-load-path \\(current-toplevel\\)\\)")
+				 "(add-to-load-path (original-current-toplevel))")
+				(("\\(add-to-load-path \\(string-append \\(current-toplevel\\) \"/lib\"\\)\\)")
+				 "(add-to-load-path (string-append (original-current-toplevel) \"/lib\"))"))		
+		   (substitute* '("artanis/tpl/parser.scm"
+				  "artanis/mvc/controller.scm"
+				  "artanis/webapi/restful.scm")			      			       
+				(("current-toplevel")
+				"original-current-toplevel"))				
+		   (substitute* "artanis/utils.scm"			      			       
+				(("\\(let\\* \\(\\(toplevel \\(current-toplevel\\)\\)")
+				 "(let* ((toplevel (original-current-toplevel))")
+				(("\\(current-toplevel\\) file\\)\\)\\)")
+				"(original-current-toplevel) file)))")
+				(("\\(if \\(current-toplevel\\)")
+				 "(if (original-current-toplevel)")
+				(("\\(format \\#f \"~a/pub/~a\" \\(current-toplevel\\) path\\)")
+				 "(format #f \"~a/pub/~a\" (original-current-toplevel) path)"))				
+		   (substitute* "artanis/env.scm"			      			       
+				(("            current-toplevel\n")
+				 "            current-toplevel\n            %original-current-toplevel\n            original-current-toplevel\n")
+				(("\\(define \\(current-toplevel\\)\n")
+					 "(define %original-current-toplevel (make-parameter #f))\n")
+				(("  \\(or \\(%current-toplevel\\)\n")
+					 "  (define (original-current-toplevel)\n")
+				(("      \\(find-ENTRY-path identity #t\\)\\)\\)\n")
+				 "     (or (%original-current-toplevel)\n         (find-ENTRY-path identity #t)))\n\n(define (current-toplevel) \"/tmp/limsn\")"))
+	;;============END forguix mods=========================================================================
 				   
                    (substitute* "artanis/artanis.scm"
                     (("[[:punct:][:space:]]+->json-string[[:punct:][:space:]]+")
@@ -179,65 +261,112 @@ more. v0.5.1 contains feature enhancements required by LIMS*Nucleus")
     (license (list license:gpl3+ license:lgpl3+)))) ;dual license
 
 
-(define-public lnpg
-(package
-  (name "lnpg")
-  (version "0.1")
-  (source (origin
-              (method url-fetch)
-              (uri "https://github.com/mbcladwell/lnpg/releases/download/v01/lnpg-0.1.tar.gz")
-              (sha256
-               (base32
-                "0mh8b7c5nywfzwpsp3b32kkipqzncz8da47i6isqy72k4xndysbd"))))
-  (build-system gnu-build-system)
+  (package
+    (name "limsn")
+    (version "0.1.0")
+   (source (origin
+            (method url-fetch)
+   ;;         (uri (string-append "file:///home/mbc/syncd/tobedeleted/limsn/limsn-0.1.tar.gz"))
+            (uri (string-append "file:///home/admin/limsn-0.1.tar.gz"))
+            (sha256
+             (base32
+              "0ssi1wpaf7plaswqqjwigppsg5fyh99vdlb9kzl7c9lng89ndq1i"))))
+   (build-system gnu-build-system)
   (arguments `(#:tests? #false ; there are none
 			#:phases (modify-phases %standard-phases
     		       (add-after 'unpack 'patch-prefix
 			       (lambda* (#:key inputs outputs #:allow-other-keys)
-				 (substitute* '("scripts/lnpg.sh"
-						"lnpg/lnpg.scm")
+				 (substitute* '("./limsn/lib/labsolns/lnpg.scm"
+						"./scripts/start-limsn.sh"
+						"./scripts/init-limsn.sh"
+						"./limsn/ENTRY")						
 						(("abcdefgh")
 						(assoc-ref outputs "out" )) )
-					#t))		    
-		       (add-before 'install 'make-scripts-dir
+				 #t))		       			       
+		(add-after 'unpack 'augment-GUILE_LOAD_PATH
+			   (lambda _
+			     (setenv "GUILE_LOAD_PATH"
+				     (string-append "./limsn/lib:"
+						    "/gnu/store/wchn5f0frrm860vnrj0f71286xbrpw91-artanis-0.5.2/share/guile/site/3.0:"
+						    "/gnu/store/780bll8lp0xvj7rnazb2qdnrnb329lbw-guile-json-3.5.0/share/guile/site/3.0:"
+						    "/gnu/store/jmn100gjcpqbfpxrhrna6gzab8hxkc86-guile-redis-2.1.1/share/guile/site/3.0:"
+						    (getenv "GUILE_LOAD_PATH")))
+			     #t))
+                       (add-before 'install 'make-lib-dir
 			       (lambda* (#:key outputs #:allow-other-keys)
 				    (let* ((out  (assoc-ref outputs "out"))
-					   (scripts-dir (string-append out "/scripts"))
-					   (bin-dir (string-append out "/bin"))
-					   (dummy (install-file "scripts/lnpg.sh" bin-dir))
+					   (lib-dir (string-append out "/share/guile/site/3.0/limsn/lib"))
+					   (dummy (mkdir-p lib-dir)))            				       
+				       (copy-recursively "./limsn/lib" lib-dir)
+				       #t)))
+		       (add-after 'unpack 'make-dir
+				   (lambda* (#:key outputs #:allow-other-keys)
+				     (let* ((out  (assoc-ref outputs "out"))
+					   (labsolns-dir (string-append out "/labsolns"))
+					   (mkdir-p labsolns-dir)
+					   (dummy (copy-recursively "./limsn/lib/labsolns" labsolns-dir))) 
+				       #t)))
+
+                       (add-before 'install 'make-scripts-dir
+			       (lambda* (#:key outputs #:allow-other-keys)
+				    (let* ((out  (assoc-ref outputs "out"))
+					   (scripts-dir (string-append out "/share/guile/site/3.0/limsn/scripts"))
+					;;   (scripts-dir (string-append out "/scripts"))
 					   (dummy (mkdir-p scripts-dir)))            				       
 				       (copy-recursively "./scripts" scripts-dir)
 				       #t)))
-		        (add-after 'install 'wrap-lnpg
+		       (add-after 'install 'make-bin-dir
 				  (lambda* (#:key inputs outputs #:allow-other-keys)
 				    (let* ((out (assoc-ref outputs "out"))
 					   (bin-dir (string-append out "/bin"))
-					    (scm  "/share/guile/site/3.0")
-					    (go   "/lib/guile/3.0/site-ccache")
-					   (dummy (chmod (string-append out "/bin/lnpg.sh") #o555 ))) ;;read execute, no write
-				      (wrap-program (string-append out "/bin/lnpg.sh")
+					   (scm  "/share/guile/site/3.0")
+					   (go   "/lib/guile/3.0/site-ccache")
+					   (dummy (install-file "./scripts/start-limsn.sh" bin-dir))					   
+					   (dummy (chmod (string-append bin-dir "/start-limsn.sh") #o555 ))
+					   (dummy (wrap-program (string-append bin-dir "/start-limsn.sh")
+								`( "PATH" ":" prefix  (,bin-dir) )))					   
+					   (dummy (install-file "./scripts/init-limsn.sh" bin-dir))
+					   (dummy (chmod (string-append bin-dir "/init-limsn.sh") #o555 ))
+					   (dummy (wrap-program (string-append bin-dir "/init-limsn.sh")
+						    `( "PATH" ":" prefix  (,bin-dir) )))
+					   (dummy (install-file "./scripts/lnpg.sh" bin-dir))					   
+					   (dummy (chmod (string-append bin-dir "/lnpg.sh") #o555 ))) ;;read execute, no write
+				      (wrap-program (string-append bin-dir "/lnpg.sh")
 						    `( "PATH" ":" prefix  (,bin-dir) )
-						     `("GUILE_LOAD_PATH" prefix
+                                                      `("GUILE_LOAD_PATH" prefix
 						       (,(string-append out scm)))						
 						     `("GUILE_LOAD_COMPILED_PATH" prefix
 						       (,(string-append out go)))
-						     )		    
-				      #t)))	     
+						    )		    
+				      #t)))
+				      				   		      				     
+				      			    		     		             
 		       )))
-  (native-inputs
-    `(("autoconf" ,autoconf)
-      ("automake" ,automake)
-      ("pkg-config" ,pkg-config)
-      ("texinfo" ,texinfo)    
-     ))
-  (inputs `(("guile" ,guile-3.0)
-	    ("gnuplot" ,gnuplot)))
-  (propagated-inputs `( ("artanis" ,artanis-051)
-			("postgresql" ,postgresql)
-		;;	("postgresql-client" ,postgresql-client)
-			))
-  (synopsis "")
-  (description "")
-  (home-page "www.labsolns.com")
-  (license license:gpl3+)))
+    (inputs
+     `(("guile" ,guile-3.0)
+       ("gnuplot" ,gnuplot)
+       ("guile-dbi" ,guile-dbi)
+         ))
+
+    (propagated-inputs
+	`(
+	  ("artanis" ,artanis-052)
+;;	  ("guile-json" ,guile-json-3)
+;;	  ("guile-redis" ,guile-redis)
+  		))
+    (native-inputs
+     `(("bash"       ,bash)         ;for the `source' builtin
+       ("pkgconfig"  ,pkg-config)
+       ("autoconf" ,autoconf)
+       ("automake" ,automake)
+       ("texinfo" ,texinfo)
+       ("util-linux" ,util-linux))) ;for the `script' command
+
+    (synopsis "Microwell Plate management Software")
+    (description "description")
+    (home-page "http://www.labsolns.com/")
+    (license (list license:gpl3+ license:lgpl3+))) ;dual license
+
+
+
 
