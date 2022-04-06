512482a8a5a6d3c74d58f75123d0cb084a34ac3f
mbcladwell <mbcladwell@gmail.com>
2022-04-06 15:51:18 -0400

    removed guix gexp

--- labsolns/limsn.scm
+++ labsolns/limsn.scm
@@ -284,7 +284,7 @@ more. v0.5.2 contains feature enhancements required by LIMS*Nucleus")
    (arguments `(#:tests? #false ; there are none
 	       #:modules
                ((ice-9 match)
-		(guix gexp)
+		
          (ice-9 pretty-print)
          ,@%gnu-build-system-modules)
 			#:phases (modify-phases %standard-phases
@@ -304,38 +304,36 @@ more. v0.5.2 contains feature enhancements required by LIMS*Nucleus")
 				 #t))
 
     		       ;;this works
-		(add-after 'unpack 'augment-GUILE_LOAD_PATH
-			   (lambda _
-			     (setenv "GUILE_LOAD_PATH"
-				     #~(string-append
-				      "./limsn/lib:"  ;;needed for libraries
-				      ".:"
-						 ;;   "/home/mbc/.guix-profile/share/guile/site/3.0:"
-				                ;;    "/gnu/store/rgydar9dfvflqqz2irgh7njj34amaxc6-glibc-utf8-locales-2.31/lib/locale/2.31:"
-				                #$(this-package-input "artanis")
-						;;    "/gnu/store/dfa7p2zvk4xlhaq1y3hsqkzpqd73ggni-artanis-0.5.2/share/guile/site/3.0:"
-						   "/gnu/store/780bll8lp0xvj7rnazb2qdnrnb329lbw-guile-json-3.5.0/share/guile/site/3.0:"
-						    "/gnu/store/jmn100gjcpqbfpxrhrna6gzab8hxkc86-guile-redis-2.1.1/share/guile/site/3.0:"
-						    "/gnu/store/3f0lv3m4vlzqc86750025arbskfrq05p-guile-dbi-2.1.8/share/guile/site/3.0:"
-						    (getenv "GUILE_LOAD_PATH")))
-			     #t))
+	;; 	(add-after 'unpack 'augment-GUILE_LOAD_PATH
+;; 			   (lambda _
+;; 			     (setenv "GUILE_LOAD_PATH"
+;; 				     (string-append
+;; 				      "./limsn/lib:"  ;;needed for libraries
+;; 				      ".:"
+;; 						 ;;   "/home/mbc/.guix-profile/share/guile/site/3.0:"
+;; 				                ;;    "/gnu/store/rgydar9dfvflqqz2irgh7njj34amaxc6-glibc-utf8-locales-2.31/lib/locale/2.31:"
+;; 				                (assoc-ref %build-inputs "artanis") ":"
+;; ;;						    "/gnu/store/dfa7p2zvk4xlhaq1y3hsqkzpqd73ggni-artanis-0.5.2/share/guile/site/3.0:"
+;; 						   "/gnu/store/780bll8lp0xvj7rnazb2qdnrnb329lbw-guile-json-3.5.0/share/guile/site/3.0:"
+;; 						    "/gnu/store/jmn100gjcpqbfpxrhrna6gzab8hxkc86-guile-redis-2.1.1/share/guile/site/3.0:"
+;; 						    "/gnu/store/3f0lv3m4vlzqc86750025arbskfrq05p-guile-dbi-2.1.8/share/guile/site/3.0:"
+;; 						    (getenv "GUILE_LOAD_PATH")))
+;; 			     #t))
 
-		       ;; (add-after 'unpack 'augment-GUILE_LOAD_PATH
-		       ;; 		  (lambda* (#:key outputs #:allow-other-keys)
-		       ;; 		    (let* ((out  (assoc-ref outputs "out"))
-		       ;; 			   (scm  "/share/guile/site/3.0"))
-		       ;; 		      (setenv "GUILE_LOAD_PATH"
-		       ;; 			      (string-append
-		       ;; 			       "./limsn/lib:"  ;;needed for libraries
-		       ;; 			       (string-append out scm ":.:") 
-		       ;; 			       ;;   "/home/mbc/.guix-profile/share/guile/site/3.0:"
-		       ;; 		               ;;    "/gnu/store/rgydar9dfvflqqz2irgh7njj34amaxc6-glibc-utf8-locales-2.31/lib/locale/2.31:"
-		       ;; 			       "/gnu/store/dfa7p2zvk4xlhaq1y3hsqkzpqd73ggni-artanis-0.5.2/share/guile/site/3.0:"
-		       ;; 			       "/gnu/store/780bll8lp0xvj7rnazb2qdnrnb329lbw-guile-json-3.5.0/share/guile/site/3.0:"
-		       ;; 			       "/gnu/store/jmn100gjcpqbfpxrhrna6gzab8hxkc86-guile-redis-2.1.1/share/guile/site/3.0:"
-		       ;; 			       "/gnu/store/3f0lv3m4vlzqc86750025arbskfrq05p-guile-dbi-2.1.8/share/guile/site/3.0:"
-		       ;; 			       (getenv "GUILE_LOAD_PATH")))
-		       ;; 		      #t)))
+		       (add-after 'unpack 'augment-GUILE_LOAD_PATH
+				  (lambda* (#:key inputs outputs #:allow-other-keys)
+				    (let* ((out  (assoc-ref outputs "out"))
+					   (scm  "/share/guile/site/3.0"))
+				      (setenv "GUILE_LOAD_PATH"
+					      (string-append
+					       "./limsn/lib:"  ;;needed for libraries
+					       (string-append out scm ":.:")
+					       (assoc-ref inputs "artanis") "/share/guile/site/3.0:"
+					       (assoc-ref inputs "guile-json") "/share/guile/site/3.0:"
+					       (assoc-ref inputs "guile-redis") "/share/guile/site/3.0:"
+					       (assoc-ref inputs "guile-dbi") "/share/guile/site/3.0:"					       
+					       (getenv "GUILE_LOAD_PATH")))
+				      #t)))
 
 
 
@@ -394,16 +392,17 @@ more. v0.5.2 contains feature enhancements required by LIMS*Nucleus")
 							     )))
 					     all-files))					   					   	    
 				      #t)))))
-  (inputs
-     `(("guile" ,guile-3.0)      
-         ))
-    (propagated-inputs
-	`(
-	  ("artanis" ,artanis-052)
-	  ("gnuplot" ,gnuplot)
-	  ("guile-dbi" ,guile-dbi)
-	  ("postgresql" ,postgresql)
-	  ("dbd-postgresql" ,guile-dbd-postgresql)
+   (inputs
+    `(("guile" ,guile-3.0)))
+   (propagated-inputs
+    `(
+      ("artanis" ,artanis-052)
+      ("gnuplot" ,gnuplot)
+      ("guile-dbi" ,guile-dbi)
+      ("postgresql" ,postgresql)
+      ("dbd-postgresql" ,guile-dbd-postgresql)
+      ("guile-json" ,guile-json-3)
+      ("guile-redis" ,guile-redis)
   		))
     (native-inputs
      `(("bash"       ,bash)         ;for the `source' builtin
@@ -419,5 +418,5 @@ more. v0.5.2 contains feature enhancements required by LIMS*Nucleus")
     (license (list license:gpl3+ license:lgpl3+)))) ;dual license
 
 
-
+limsn
 
