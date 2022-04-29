e2ad5cbdd38ef2e55911a8e5966de1581f384819
mbcladwell <mbcladwell@gmail.com>
2022-04-29 16:14:08 -0400

    changed to guile-json to get 4.5

--- labsolns/guile-oauth.scm
+++ labsolns/guile-oauth.scm
@@ -1,4 +1,4 @@
-(define-module (labsolns xkguile-oauth)
+(define-module (labsolns guile-oauth)
  #:use-module ((guix licenses) #:prefix license:)
  #:use-module (gnu packages autotools)
  #:use-module (gnu packages guile)
