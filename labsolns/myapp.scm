73e256e32060e95d25270cb6b25e61fc162d072a
mbcladwell <mbcladwell@gmail.com>
2022-08-28 06:48:11 -0400

    autocommit

--- labsolns/myapp.scm
+++ labsolns/myapp.scm
@@ -258,7 +258,7 @@ more. v0.5.1 contains feature enhancements required by LIMS*Nucleus")
 	    (uri (string-append "https://github.com/mbcladwell/myapp/releases/download/v0.1/myapp-0.1.tar.gz"))	    
             (sha256
              (base32
-             "14sdfvdcc975sk1n2f16n8rkdhlwqfj8rh3hv5i1gykc6xcq9d42"))))
+             "13sk7p3kqszsad73ix4cnq619ghp1f9why348slszix8f4sxlmjj"))))
    (build-system gnu-build-system)
   (arguments `(#:tests? #false ; there are none
 			#:phases (modify-phases %standard-phases
