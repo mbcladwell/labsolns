99280ce4888c591eb34a0b9cd113a641eb328a50
mbcladwell <mbcladwell@gmail.com>
2023-09-11 14:42:01 -0400

    autocommit

--- labsolns/conmanv4.scm
+++ labsolns/conmanv4.scm
@@ -1,15 +1,15 @@
-(use-modules
-  (guix packages)
-  ((guix licenses) #:prefix license:)
-  (guix download)
-  (guix build-system gnu)
-  (gnu packages)
-  (gnu packages autotools)
-  (gnu packages guile)
-  (gnu packages guile-xyz)
-  (gnu packages pkg-config)
-  (gnu packages texinfo)
-  (gnutls))
+(define-module (conmanv4)
+   #:use-module (guix packages)
+   #:use-module ((guix licenses) #:prefix license:)
+   #:use-module (guix download)
+   #:use-module (guix build-system gnu)
+   #:use-module (gnu packages)
+   #:use-module (gnu packages autotools)
+   #:use-module (gnu packages guile)
+   #:use-module (gnu packages guile-xyz)
+   #:use-module (gnu packages pkg-config)
+   #:use-module (gnu packages texinfo)
+   #:use-module (gnutls))
 
 (define-public conmanv4
              (let ((commit "81625acc3f703da4a12d5632a73ebba6f8e5fe54")
@@ -26,38 +26,71 @@
               (sha256
              (base32 "0l264qscqwj2fkqz50mq4vnkcny18bnlwp4gvrvg8ykc81kw3d90"))))
     (build-system guile-build-system)
+    (arguments `(
+		 #:phases (modify-phases %standard-phases
+    				       (add-after 'unpack 'patch-prefix
+						  (lambda* (#:key inputs outputs #:allow-other-keys)
+						    (let ((out  (assoc-ref outputs "out")))					  
+						      (substitute* '("conmanv4/env.scm")
+								   (("ebbotstorepath")
+								    out))
+						      (substitute* '("scripts/conman.sh")
+								   (("guileloadpath")
+								    (string-append  out "/share/guile/site/3.0:"
+										    (assoc-ref inputs "guile")  "/share/guile/site/3.0:"
+										    (assoc-ref inputs "gnutls")  "/share/guile/site/3.0:"
+										    (getenv "GUILE_LOAD_PATH") "\"")))
 
-  (arguments `(#:tests? #false ; there are none
-	       #:phases (modify-phases %standard-phases
-       		       (add-after 'unpack 'patch-prefix
-			       (lambda* (#:key inputs outputs #:allow-other-keys)
-					(substitute* "conmanv4/env.scm"
-						(("abcdefgh")
-						(assoc-ref outputs "out" )) )
-					#t))
-		       (add-before 'install 'make-dir
-			       (lambda* (#:key outputs #:allow-other-keys)
-				    (let* ((out  (assoc-ref outputs "out"))
+						      (substitute* '("scripts/conman.sh")
+					(("guileexecutable")
+					 (string-append (assoc-ref inputs "guile") "/bin/guile")))
+				      
+				      (substitute* '("scripts/conman.sh")
+					(("guileloadcompiledpath")
+					 (string-append  out "/lib/guile/3.0/site-ccache:"
+							 (assoc-ref inputs "guile")  "/lib/guile/3.0/site-ccache:"
+							 (assoc-ref inputs "gnutls")  "/lib/guile/3.0/site-ccache:"
+							 (getenv "GUILE_LOAD_COMPILED_PATH") "\""))))
+				    #t))		    
+		       (add-after 'patch-prefix 'make-dir
+			 (lambda* (#:key outputs #:allow-other-keys)
+			   (let* ((out  (assoc-ref outputs "out"))
+				  (conman-dir (string-append out "/share/guile/site/3.0/ebbot"))
+				  (mkdir-p conman-dir)
+				  (dummy (copy-recursively "./conmanv4" conman-dir))) 
+			     #t)))
+		       
+			   (add-after 'make-dir 'make-bin-dir
+				  (lambda* (#:key inputs outputs #:allow-other-keys)
+				    (let* ((out (assoc-ref outputs "out"))
 					   (bin-dir (string-append out "/bin"))
-					   (dummy (mkdir-p bin-dir))
-					   )            				       
-				       (copy-recursively "./bin" bin-dir)
-				       #t)))
-		       (add-before 'install 'make-dir
-			       (lambda* (#:key outputs #:allow-other-keys)
-				    (let* ((out  (assoc-ref outputs "out"))
-					   (scripts-dir (string-append out "/scripts"))
-					   (dummy (mkdir-p scripts-dir))
-					   )            				       
-				       (copy-recursively "./scripts" scripts-dir)
-				       #t))))))
+					   (scm  "/share/guile/site/3.0")
+					   (go   "/lib/guile/3.0/site-ccache")
+					   (all-files '("conman.sh")))				      
+				      (map (lambda (file)
+					     (begin
+					       (install-file (string-append "./scripts/" file) bin-dir)
+					       (chmod (string-append bin-dir "/" file) #o555 ) ;;read execute, no write
+					       (wrap-program (string-append bin-dir "/" file)
+							     `( "PATH" ":" prefix  (,bin-dir) )							     
+							     `("GUILE_LOAD_PATH" prefix
+							       (,(string-append out scm)
+								))
+							     `("GUILE_LOAD_COMPILED_PATH" prefix
+							       (,(string-append out go)))
+							     )))
+					     all-files))					   					   	    
+				      #t))
+
+		       )))
   (native-inputs
    `(
       ("texinfo" ,texinfo)))
-  (inputs `(("guile" ,guile-3.0)))
+  (inputs `(("guile" ,guile-3.0)
+	    ("gnutls" ,gnutls)))
   (propagated-inputs `())
   (synopsis "")
   (description "")
   (home-page "www.labsolns.com")
-  (license license:gpl3+))
+  (license license:gpl3+))))
 
