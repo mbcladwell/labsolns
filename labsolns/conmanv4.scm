c19f09d23fb7bf0040f4ae90e08cb816e6799b88
mbcladwell <mbcladwell@gmail.com>
2023-09-12 08:39:09 -0400

    autocommit

--- labsolns/conmanv4.scm
+++ labsolns/conmanv4.scm
@@ -53,7 +53,6 @@
 								       (assoc-ref inputs "guile")  "/lib/guile/3.0/site-ccache:"
 								       ;; (assoc-ref inputs "gnutls")  "/lib/guile/3.0/site-ccache:"
 								       (getenv "GUILE_LOAD_COMPILED_PATH") "\""))))
-					 )
 				    #t))		    
 		       (add-after 'patch-prefix 'make-dir
 			 (lambda* (#:key outputs #:allow-other-keys)
