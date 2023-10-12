569b8d4cc4dd33da05abb4653ed97d4c12a6b547
mbcladwell <mbcladwell@gmail.com>
2023-10-12 09:54:37 -0400

    autocommit

--- labsolns/conmanv4.scm
+++ labsolns/conmanv4.scm
@@ -17,7 +17,7 @@
    )
 
 (define-public conmanv4
-             (let ((commit "1d120ce69ccc02cce389b64f11e81a7ae69fd75c")
+             (let ((commit "2da88acf2fc786c7d139a5ff91de95cdcb1ef6a8")
         (revision "2"))
   (package
     (name "conmanv4")
@@ -29,7 +29,7 @@
              (commit commit)))
               (file-name (git-file-name name version))
               (sha256
-             (base32 "1hdq037dw806db15vwql5rimrwdg2rbkb7zzj0dxircx726m3rl5"))))
+             (base32 "1c88p896q498w5bm2z6m8if1yxbpnipvy6vrph97357wh69d1b3k"))))
     (build-system guile-build-system)
     (arguments `(
 		 #:phases (modify-phases %standard-phases
