3c7fbbcd5b9a0ba5a0bfef39c87b9152c973f200
mbcladwell <mbcladwell@gmail.com>
2024-06-18 06:49:32 -0400

    guile-oauth2

--- labsolns/guile-oauth2.scm
+++ labsolns/guile-oauth2.scm
@@ -36,7 +36,7 @@
      `(("guile" ,guile-3.0)))
     (propagated-inputs
      (list
-        guile-json       
+        guile-json-4       
         guile-gcrypt
        ;;       ("gnutls" ,gnutls)
        ))
