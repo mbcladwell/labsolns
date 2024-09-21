76bb4ce89f19f4c0b666ed44aa33c8bc1545f4c8
mbcladwell <mbcladwell@gmail.com>
2024-09-21 06:53:20 -0400

    autocommit

--- labsolns/ebbot.scm
+++ labsolns/ebbot.scm
@@ -102,7 +102,7 @@
   (native-inputs
     `(("guile" ,guile-3.0)))
   (propagated-inputs `( ("guile-json" ,guile-json-4) ("guile-oauth" ,guile-oauth)("bash" ,bash)
-			("guile-gnutls" ,guile-gnutls)
+			("gnutls" ,gnutls)
 	       ;;			("artanis" ,artanis-07)
 			))
   (synopsis "Auto tweeter for educational tweets concerning propaganda")
