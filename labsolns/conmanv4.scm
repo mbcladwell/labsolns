eebb852162241d6e495ebfd3fbfe0a1fc48adbb4
mbcladwell <mbcladwell@gmail.com>
2025-01-10 15:42:51 -0500

    autocommit

--- labsolns/conmanv4.scm
+++ labsolns/conmanv4.scm
@@ -9,6 +9,7 @@
    #:use-module (guix build-system gnu)
    #:use-module (gnu packages bash)
    #:use-module (gnu packages)
+   #:use-module (gnu packages tls)
    #:use-module (gnu packages autotools)
    #:use-module (gnu packages guile)
    #:use-module (gnu packages guile-xyz)
