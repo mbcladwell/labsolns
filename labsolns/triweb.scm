716f7edefe09d37afe6bd2283acb826fab85e0dc
mbcladwell <mbcladwell@gmail.com>
2025-03-03 08:53:16 -0500

    autocommit

---
+++ labsolns/triweb.scm
@@ -0,0 +1,47 @@
+(define-module (labsolns triweb)
+  #:use-module (guix packages)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix download)
+  #:use-module (guix git-download)
+  #:use-module (guix build-system gnu)
+  #:use-module (guix utils)
+  #:use-module (gnu packages autotools)
+  #:use-module (gnu packages)
+  #:use-module (gnu packages bash)
+  #:use-module (gnu packages guile)
+  #:use-module (guix build-system guile)
+  #:use-module (gnu packages guile-xyz)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages texinfo)
+;;  #:use-module (labsolns guile-oauth)
+;;  #:use-module (labsolns artanis-122)
+;;  #:use-module (json)
+
+  )
+
+(define-public triweb
+             (let ((commit "0f610bd0f3cc230ab33982683cb4effa44a673f6")
+        (revision "4"))
+(package
+  (name "triweb")
+  (version (string-append "0.1." (string-take commit 7)))
+  (source (origin
+           (method git-fetch)
+                (uri (git-reference
+                      (url "https://github.com/mbcladwell/triweb")
+                      (commit commit)))
+                        (file-name (git-file-name name version))
+                (sha256 
+             (base32 "0z730396nkvjhxdj9ndl31hgzmn8qxiaydg4b760214dvw5vc03k"))))
+  (build-system guile-build-system)
+  
+  (native-inputs
+    `(("guile" ,guile-3.0)))
+  (propagated-inputs `(("bash" ,bash)("limsn" ,limsn)("conman" ,conmanv5)("babweb" ,babweb)))
+  (synopsis "AWS setup providing limsn, artanis, babweb, and conman")
+  (description "AWS setup providing limsn, artanis, babweb, and conman")
+  (home-page "www.labsolns.com")
+  (license license:gpl3+))))
+
+
+
