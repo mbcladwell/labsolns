142527e2a99c76a7ef7434875735678aa5afc905
mbcladwell <mbcladwell@gmail.com>
2025-04-16 08:09:12 -0400

    autocommit

--- labsolns/myapp3.scm
+++ labsolns/myapp3.scm
@@ -18,7 +18,7 @@
   #:use-module ((srfi srfi-1) #:select (alist-delete)))
 
 (define-public myapp3
-             (let ((commit "cf952dba5ecdc96402e54881d20cfe70a146b091")
+             (let ((commit "595200ee251347b7f7b3c839d44c4402d91f1c17")
         (revision "4"))
 
   (package
@@ -31,7 +31,7 @@
 		    (commit commit)))
 	      (file-name (git-file-name name version))
 	      (sha256
-	             (base32 "0d6q80jpjij4f04awbpdb01qzvm5vl55qccjzb0zn9j1aah9p6k5"))))
+	             (base32 "16wblk16ppk4px0ppq726lja5ydj1kvgyjimjb9wifvmawd3dxcb"))))
     (build-system guile-build-system)
 
 (arguments `(
